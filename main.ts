5631c18b9f172f53d8dbd92379f3ca7b80e3fe46
jordicollell <78138546+jordicollell@users.noreply.github.com>
2021-03-26 00:03:39 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,24 @@
 input.onPinPressed(TouchPin.P0, function () {
     comptador += 1
+    if (comptador == 1) {
+        temps_inici = input.runningTime()
+    }
     temps_final = input.runningTime()
 })
+// Posar zero
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     comptador = 0
     temps_final = 0
 })
-let temps_inici = 0
 let temps_final = 0
+let temps_inici = 0
 let comptador = 0
 comptador = 0
 basic.clearScreen()
 basic.forever(function () {
     if (comptador == 10) {
-        if (temps_final - temps_inici < 7000) {
+        if (temps_final - temps_inici < 3000) {
             basic.showLeds(`
                 # # # # #
                 # # # # #
@@ -31,14 +35,5 @@ basic.forever(function () {
                 # . . . #
                 `)
         }
-    }
-})
-basic.forever(function () {
-    serial.writeLine("" + (temps_final - temps_inici))
-    basic.pause(500)
-})
-basic.forever(function () {
-    if (comptador == 1) {
-        temps_inici = input.runningTime()
     }
 })
