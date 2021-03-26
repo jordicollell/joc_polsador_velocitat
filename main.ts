f3d658d81abb030d84b9b9ef974ca877218381a3
jordicollell <78138546+jordicollell@users.noreply.github.com>
2021-03-26 10:17:55 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,28 +11,58 @@ input.onButtonPressed(Button.A, function () {
     comptador = 0
     temps_final = 0
 })
+let temps = 0
 let temps_final = 0
 let temps_inici = 0
 let comptador = 0
 comptador = 0
 basic.clearScreen()
 basic.forever(function () {
-    if (comptador == 10) {
-        if (temps_final - temps_inici < 3000) {
+    temps = temps_final - temps_inici
+    if (comptador == 6) {
+        if (temps > 3000) {
             basic.showLeds(`
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . .
                 `)
-        } else {
+        }
+        if (temps < 3000 && temps >= 2500) {
             basic.showLeds(`
-                # . . . #
-                . # . # .
+                . . . . .
+                . . . . .
+                . . . . .
+                . # . . .
+                # # . . .
+                `)
+        }
+        if (temps < 2500 && temps >= 2000) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
                 . . # . .
-                . # . # .
-                # . . . #
+                . # # . .
+                # # # . .
+                `)
+        }
+        if (temps < 2000 && temps >= 1500) {
+            basic.showLeds(`
+                . . . . .
+                . . . # .
+                . . # # .
+                . # # # .
+                # # # # .
+                `)
+        }
+        if (temps < 1500) {
+            basic.showLeds(`
+                . . . . #
+                . . . # #
+                . . # # #
+                . # # # #
+                # # # # #
                 `)
         }
     }
